8d5a2151c3db9abbbd4b2cb3f541eb0523ea010b
“Camila <cami.rom16@gmail.com>
2023-08-30 20:04:27 -0400

    Login component FE and BE

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
 const Login = () => {
-    const { accountId } = useParams();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
